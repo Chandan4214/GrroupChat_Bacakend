f285810deb0460f52a0164e760684628b98a1927
Chandan <cs762585@gmail.com>
2025-11-09 12:49:26 +0530

    updated cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://group-chat-frontend-65dp.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
